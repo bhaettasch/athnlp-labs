ba41c20bdd0356de037d8a4bd559f8404c5d5707
Benjamin Hättasch <benjamin.haettasch@cs.tu-darmstadt.de>
2019-09-21 14:23:14 +0300

    Lab 1: Refactored, split into different classes

--- 1_perceptron_pos.py
+++ 1_perceptron_pos.py
@@ -1,6 +1,5 @@
 import random
-
-import numpy as np
+import abc
 
 from nltk import ConditionalFreqDist
 from tqdm import tqdm
@@ -10,7 +9,94 @@ from athnlp.readers.brown_pos_corpus import BrownPosTag
 EPOCHS = 3
 
 
-class PerceptronPOSTagger:
+class POSTagger:
+    def __init__(self):
+        self.name = "POS Tagger"
+        self.corpus = corpus
+        self.pos_tags = corpus.dictionary.y_dict.names
+        self.vocab_size = len(corpus.dictionary.x_dict)
+
+    @abc.abstractmethod
+    def _get_word_feature(self, word, sent):
+        """
+        Encode the given word (using the sent information if applicable)
+
+        :param word: word to encode
+        :type word: str
+        :param sent: context (sent the word is from)
+        :type sent: Sequence
+        :return: encoded word
+        :rtype: Any
+        """
+        pass
+
+    @abc.abstractmethod
+    def _get_prediction(self, word):
+        """
+        Get a prediction for the given word
+
+        :param word: word to get tag for
+        :type word: str
+        :return: tag prediction
+        :rtype: str
+        """
+        pass
+
+    @abc.abstractmethod
+    def train(self):
+        """
+        Train tagger model on the given corpus
+        """
+        pass
+
+    def evaluate(self):
+        """
+        Evaluate the tagger
+        :return: list of dataset-acurracy-tuples
+        :rtype: list[(str, float)]
+        """
+        accuracies = []
+
+        # Evaluate all data splits
+        for (split_name, split_data) in [("Train", self.corpus.train), ("Dev", self.corpus.dev), ("Test", self.corpus.test)]:
+            # Loop over all sentences and all words in it and count the share of correct predictions
+            total = 0
+            correct = 0
+            for sent in tqdm(split_data, desc=f"Evaluating {split_name}"):
+                for (word, gold_tag) in sent.get_tag_word_tuples():
+                    total += 1
+                    correct += self._get_prediction(self._get_word_feature(word, sent)) == gold_tag
+            accuracies.append((split_name, correct / total * 100))
+
+        accuracies_string = ' '.join(f"{name}: {acc:3.2f}%" for name, acc in accuracies)
+        print(f"Accuracies:\n{accuracies_string}")
+        return accuracies
+
+
+class MajorityClassPOSTagger(POSTagger):
+
+    def __init__(self, corpus):
+        super().__init__()
+        self.name = "Majority Class POS Tagger"
+        self.word_pos_cfd = None
+
+    def train(self):
+        """
+        This trains a simple baseline which just uses majority class voting for every word in vocabulary
+        disregarding of its context
+        """
+        self.word_pos_cfd = ConditionalFreqDist(tp for seq_list in self.corpus.train for tp in seq_list.get_tag_word_tuples())
+
+    def _get_word_feature(self, word, sent):
+        return word
+
+    def _get_prediction(self, word):
+        if word in self.word_pos_cfd:
+            return self.word_pos_cfd[word].most_common(1)[0][0]
+        return ""
+
+
+class UnigramPerceptronPOSTagger(POSTagger):
 
     def __init__(self, corpus):
         """
@@ -19,10 +105,8 @@ class PerceptronPOSTagger:
         :param corpus: the corpus to train and evaluate on
         :type corpus: BrownPosTag
         """
-        self.corpus = corpus
-        self.pos_tags = corpus.dictionary.y_dict.names
-        self.vocab_size = len(corpus.dictionary.x_dict)
-        self.baseline_cfd = None
+        super().__init__()
+        self.name = "Unigram Perceptron POS Tagger"
         self.weights_per_label = {}
 
     def train(self):
@@ -40,9 +124,9 @@ class PerceptronPOSTagger:
             random.shuffle(self.corpus.train)
             for sent in tqdm(self.corpus.train, desc=f"Epoch {epoch}"):
                 for (word, tag) in sent.get_tag_word_tuples():
-                    feature = self._get_word_feature(word)
+                    feature = self._get_word_feature(word, None)
                     # Get prediction
-                    pred_tag = self._get_prediction(feature, self.weights_per_label)
+                    pred_tag = self._get_prediction(feature)
                     # If there was a misprediction
                     if pred_tag != tag:
                         # Update both affected weights
@@ -51,14 +135,14 @@ class PerceptronPOSTagger:
                             self.weights_per_label[pred_tag][f] = self.weights_per_label[tag].get(f, 0) - 1
                         mistakes += 1
 
-    def _get_word_feature(self, word):
+    def _get_word_feature(self, word, sent):
         return [word]
 
-    def _get_prediction(self, feature, model_weights):
+    def _get_prediction(self, feature):
         best_label = ""
         best_score = -1
 
-        for (label, weights) in model_weights.items():
+        for (label, weights) in self.weights_per_label.items():
             score = sum(weights.get(f, 0) for f in feature)
             if score > best_score:
                 best_label = label
@@ -66,56 +150,14 @@ class PerceptronPOSTagger:
 
         return best_label
 
-    def evaluate(self):
-        total = 0
-        correct = 0
-        for sent in tqdm(self.corpus.dev, desc="Evaluating"):
-            for (word, gold_tag) in sent.get_tag_word_tuples():
-                total += 1
-                predicted_tag = self._get_prediction(self._get_word_feature(word), self.weights_per_label)
-                if predicted_tag == gold_tag:
-                    correct += 1
-
-        baseline_accuracy = correct / total * 100
-        print(f"Simple Perceptron Accuracy: {baseline_accuracy:3.2f}%")
-
-    def train_baseline(self):
-        """
-        This trains a simple baseline which just uses majority class voting for every word in vocabulary
-        disregarding of its context
-
-        :return:
-        :rtype:
-        """
-        self.baseline_cfd = ConditionalFreqDist(tp for seq_list in self.corpus.train for tp in seq_list.get_tag_word_tuples())
-
-    def _get_tag_for_word_baseline(self, word):
-        return
-
-    def evaluate_baseline(self):
-        """
-        Evaluate accuracy of the baseline
-        """
-        total = 0
-        correct = 0
-        for sent in tqdm(self.corpus.dev, desc="Evaluating"):
-            for (word, gold_tag) in sent.get_tag_word_tuples():
-                total += 1
-                # Known word?
-                if word in self.baseline_cfd:
-                    # Get majority class for this tag
-                    predicted_tag = self.baseline_cfd[word].most_common(1)[0][0]
-                    if predicted_tag == gold_tag:
-                        correct += 1
-
-        baseline_accuracy = correct / total * 100
-        print(f"Baseline (Majority Class Voting) Accuracy: {baseline_accuracy:3.2f}%")
-
 
 if __name__ == "__main__":
+    print("Loading corpus")
     corpus = BrownPosTag()
-    pos_tagger = PerceptronPOSTagger(corpus)
-    pos_tagger.train_baseline()
-    pos_tagger.evaluate_baseline()
-    pos_tagger.train()
-    pos_tagger.evaluate()
+
+    print("Testing models")
+    for tagger_type in [MajorityClassPOSTagger, UnigramPerceptronPOSTagger]:
+        tagger = tagger_type(corpus)
+        print(f"\n\n==={tagger.name}===\n")
+        tagger.train()
+        tagger.evaluate()
